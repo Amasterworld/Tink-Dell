19e693724f44c8989202500b3bbd1fdebe781ed4
Amasterworld <nvthao147@gmail.com>
2022-07-25 17:09:53 +0200

    Initial Commit

--- Tink-Dell/main.cpp
+++ Tink-Dell/main.cpp
@@ -2,13 +2,26 @@
 //  main.cpp
 //  Tink-Dell
 //
-//  Created by Phuong Duong Bich on 25.07.22.
+//  Created by 25.07.22.
 //
 
-#include <iostream>
+#include "interval_map.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    interval_map<char, char> m('A');
+    m.assign('a', 'g', 'G');
+   // m.assign(1, 3, 'C');
+    //m.print();
+ //   m.assign(7, 9, 'D');
+    //m.print();
+  //  m.assign(3, 10, 'C');
+    //m.print();
+    //m.assign(0, 9, 'E');
+    //m.assign(9, 11, 'E');
+    for (int i = -2; i <= 10; ++i)
+    std::cout <<  i << ' ' << m[i] << '\n';
+    m.print();
+    
     return 0;
 }
