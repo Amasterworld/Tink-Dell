d272947d44fac98f6fb2a65f0d79959466085e4b
Amasterworld <nvthao147@gmail.com>
2022-07-25 07:59:47 +0200

    Initial Commit

---
+++ Tink-Dell/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tink-Dell
+//
+//  Created by Phuong Duong Bich on 25.07.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
